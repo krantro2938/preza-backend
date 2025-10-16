7ccf6bbec7c33c682428c66c23ce4682ba4bbd4e
mrHalinNikita <halinn484@gmail.com>
2025-10-16 21:53:25 +0300

    feat: добавлен импорт env

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from .api import endpoints
 from .database import Base, engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Perio Presentation Generator")
-
 app.include_router(endpoints.router)
